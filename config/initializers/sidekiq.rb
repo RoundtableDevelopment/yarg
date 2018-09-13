aa2b20ae931dd91679a0ef30933378157045c883
dmcbrayer <dmcbrayer@gmail.com>
2018-09-12 23:34:18 -0400

    Improves heroku-centered sidekiq config.
    Setus up an initializer for sidekiq and sets
    a low concurrency for heroku deployments.

---
+++ config/initializers/sidekiq.rb
@@ -0,0 +1,12 @@
+# Using a separate shared redis database here than the cache
+# store uses for a small bit of isolation.  This won't have any
+# performance benefits but it may be a little bit easier to
+# copy one of those to a separate redis instance if it's necessary
+# in the future.
+Sidekiq.configure_server do |config|
+  config.redis = { url: "#{ENV.fetch('REDIS_URL')}/1" }
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = { size: 3, url: "#{ENV.fetch('REDIS_URL')}/1" }
+end
