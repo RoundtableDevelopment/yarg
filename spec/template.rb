f73fca0f6df19d77556308d58196202e144afad4
dmcbrayer <dmcbrayer@gmail.com>
2019-06-21 07:48:53 -0400

    Adds webmock and removes database cleaner.

--- spec/template.rb
+++ spec/template.rb
@@ -1,7 +1,6 @@
 copy_file 'spec/rails_helper.rb'
 copy_file 'spec/spec_helper.rb'
 
-copy_file 'spec/support/database_cleaner.rb'
 copy_file 'spec/support/factory_bot.rb'
 copy_file 'spec/support/vcr.rb'
 copy_file 'spec/system/view_root_path_spec.rb'
